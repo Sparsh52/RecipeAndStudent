594848a044f06aaed3df70c7896f6475233345da
Sparsh52 <chawlasparsh294@gmail.com>
2023-11-23 16:35:45 +0530

    trials-5

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!ld0)yuav=ry2=10)giv_ayj=09fbc703pup6tn2&p1m4q=*@(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # AUTH_USER_MODEL='accounts.Customuser'
 
